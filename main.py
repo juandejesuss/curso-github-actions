f55b8ce5fe1851bbeb9b9dacadc2384054e84f5b
Jesús Ayala <jesus.ayala.pt@gmail.com>
2026-06-21 00:02:21 -0600

    se corrige script de python

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 def main():
     nombre = os.getenv("USERNAME")
-    printf(f"hola, {nombre} desde Github")
+    print(f"hola, {nombre} desde Github")
 
 if __name__ == "__main__":
     main()
